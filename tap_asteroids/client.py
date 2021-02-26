837aedd0d198e351b2fb5bbfa0f37d2baea1bf04
Ross Perry <perryr16@gmail.com>
2021-02-26 13:30:20 -0700

    dotenv working

--- tap_asteroids/client.py
+++ tap_asteroids/client.py
@@ -1,12 +1,11 @@
 import json
 import requests
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
 class AsteroidClient:
     BASE_URL = 'https://api.nasa.gov/neo/rest/v1'
-    apikey = os.getenv('NASA_KEY')
+
+    def __init__(self, config):
+        self.apikey = config['apikey']
 
     def get_browse_neos(self, page_num = 0):
         url = f'{self.BASE_URL}/neo/browse?api_key={self.apikey}&page={page_num}'
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,17 @@
 from tap_asteroids import __version__
 import pytest, os 
 from tap_asteroids.client import AsteroidClient
+from dotenv import load_dotenv
+
+load_dotenv()
+config = {
+    "apikey": os.getenv('NASA_KEY'),
+}
 
 def test_version():
     assert __version__ == '0.1.0'
 
-
 def test_get_browse_neos():
-    client = AsteroidClient()
+    client = AsteroidClient(config=config)
     data = client.get_browse_neos()
     breakpoint()    
