527511fae3e708598de23c3eb2d2bede0af989ec
Ross Perry <perryr16@gmail.com>
2021-03-01 08:25:06 -0700

    update git ignore

---
+++ tap_asteroids/streams.py
@@ -0,0 +1,39 @@
+import singer 
+
+class Stream:
+    tap_stream_id           = None
+    key_properties          = []
+    replication_method      = ''
+    valid_replication_keys  = []
+    replication_key         = 'last_updated_at'
+    object_type             = ''
+    selected                = True
+
+    def __init__(self, client, state):
+        self.client = client
+        self.state = state 
+    
+    def sync(self, *args, **kwargs):
+        raise NotImplementedError("Sync of child class not implemented")
+
+class CalalogStream(Stream):
+    replication_method: 'INCREMENTAL'
+
+class FullTableStream(Stream):
+    replication_method: 'FULL_TABLE'
+
+class BrowseNeos(CatalogStream):
+    tap_stream_id = 'browse_neos'
+    key_properties = ['ID']
+    object_type = 'BrowseNeos'
+
+    def get_all_asteroids(page_num = 0):
+        pass
+
+class FeedNeos(CalalogStream):
+    tap_stream_id = 'feed_neos'
+    key_properties = ['ID']
+    object_type = 'FeedNeos'
+
+    def get_1month_asteroids(start_date, end_date):
+        pass
