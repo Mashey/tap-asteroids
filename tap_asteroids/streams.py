90bde809b5ca55067bda79fb3a40a64a1cc93e20
Ross Perry <perryr16@gmail.com>
2021-03-01 09:39:04 -0700

    testing 1month stream

--- tap_asteroids/streams.py
+++ tap_asteroids/streams.py
@@ -1,4 +1,5 @@
 import singer 
+import datetime, calendar, math, json
 
 class Stream:
     tap_stream_id           = None
@@ -27,13 +28,53 @@ class BrowseNeos(CatalogStream):
     key_properties = ['ID']
     object_type = 'BrowseNeos'
 
-    def get_all_asteroids(page_num = 0):
-        pass
+    def get_all_asteroids(self, page_limit = None):
+        nasa_data = self.client.get_browse_neos()
+        nasa_pages = nasa_data['page']['total_pages']
+        total_pages = page_limit if page_limit and nasa_pages > page_limit else nasa_pages
+        results = {'near_earth_objects': []}
+
+        for page in range(total_pages):
+            nasa_data = self.client.get_browse_neos(page)
+
+            for asteroid in nasa_data['near_earth_objects']:
+                results['near_earth_objects'].append(asteroid)
+        
+        return results
+
+
+
+
 
 class FeedNeos(CatalogStream):
     tap_stream_id = 'feed_neos'
     key_properties = ['ID']
     object_type = 'FeedNeos'
 
-    def get_1month_asteroids(start_date, end_date):
-        pass
+    def get_1month_asteroids(self, start_date):
+        date_list = start_date.split('-')
+        year = int(date_list[0])
+        month = int(date_list[1])
+        month_days = calendar.monthrange(year, month)[1]
+        num_intervals = math.ceil(month_days/7)
+
+        intervals = []
+
+        for i in range(num_intervals):
+            start = i * 7 + 1 
+            end = start + 6 
+            if end > month_days: end = month_days
+            start_interval = f'{start_date}-{str(start).zfill(2)}'
+            end_interval = f'{start_date}-{str(end).zfill(2)}'
+            intervals.append([start_interval, end_interval])
+        
+        results = {'month': start_date, 'element_count':0, 'near_earth_objects':{}}
+
+        for interval in intervals:
+            nasa_data = self.client.get_feed_neos(interval[0], interval[1])
+            results['element_count'] += nasa_data['element_count']
+
+            for k, v in nasa_data['near_earth_objects'].items():
+                results['near_earth_objects'][k] = v
+
+        return results
--- tests/test_streams.py
+++ tests/test_streams.py
@@ -1,24 +1,49 @@
-# from tap_asteroids import __version__
-# import pytest, os 
-# from tap_asteroids.streams import BrowseNeos, FeedNeos
-# from tap_asteroids.client import AsteroidClient
-# from dotenv import load_dotenv
+from tap_asteroids import __version__
+import pytest, os 
+from tap_asteroids.streams import BrowseNeos, FeedNeos
+from tap_asteroids.client import AsteroidClient
+from dotenv import load_dotenv
 
-# load_dotenv()
-# config = {
-#     "apikey": os.getenv('NASA_KEY'),
-# }
+load_dotenv()
+config = {
+    "apikey": os.getenv('NASA_KEY'),
+}
 
-# def test_version():
-#     assert __version__ == '0.1.0'
+def test_version():
+    assert __version__ == '0.1.0'
 
 
-# # @pytest.mark.vcr()
-# def test_get_all_asteroids():
-#     client = AsteroidClient(config=config)
-#     state = None
-#     stream = BrowseNewos(client=client, state=state)
-#     response = stream.get_all_asteroids(page_num=1)
-#     breakpoint()
+@pytest.mark.vcr()
+def test_get_all_asteroids():
+    client = AsteroidClient(config = config)
+    state = None
+    stream = BrowseNeos(client = client, state = state)
+    response = stream.get_all_asteroids(page_limit = 5)
+    
+    assert 100 == len(response['near_earth_objects'])
 
-#     assert 3 == len(response.keys())
+    asteroid = response['near_earth_objects'][0]
+    assert 'links' in asteroid
+    assert 'id' in asteroid
+    assert 'neo_reference_id' in asteroid
+    assert 'name' in asteroid
+    assert 'name_limited' in asteroid
+    assert 'designation' in asteroid
+    assert 'nasa_jpl_url' in asteroid
+    assert 'absolute_magnitude_h' in asteroid
+    assert 'estimated_diameter' in asteroid
+    assert 'is_potentially_hazardous_asteroid' in asteroid
+    assert 'close_approach_data' in asteroid
+    assert 'orbital_data' in asteroid
+    assert 'is_sentry_object' in asteroid
+
+@pytest.mark.vcr()
+def test_get_1month_asteroids():
+    client = AsteroidClient(config = config)
+    state = None 
+    stream = FeedNeos(client = client, state = state)
+    response = stream.get_1month_asteroids('2020-01')
+
+    assert 'month' in response
+    assert 'element_count' in response 
+    assert 'near_earth_objects' in response
